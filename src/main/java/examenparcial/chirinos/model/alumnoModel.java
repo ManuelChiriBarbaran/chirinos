3f86e00194a1ee01b0fe78c5d90b40f589f45e4b
Manuel <manuelenriquebarbaran@gmail.com>
2024-11-18 14:27:10 -0500

    cambios

--- src/main/java/examenparcial/chirinos/model/alumnoModel.java
+++ src/main/java/examenparcial/chirinos/model/alumnoModel.java
@@ -1,7 +1,11 @@
 package examenparcial.chirinos.model;
 
+import java.math.BigDecimal;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,10 +13,14 @@ import jakarta.persistence.Table;
 @Table(name = "alumno")
 public class alumnoModel {
     @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "idalumno")
     public Integer idalumno;
 
     @Column(name = "nombre")
     public String nombre;
+
+    @Column(name = "nota", columnDefinition = "DECIMAL(10,2)")
+   public BigDecimal nota;
+
 }
