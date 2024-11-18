474df966bf3b4f9c44b90411c20ae8ad0fde6cb8
Manuel <manuelenriquebarbaran@gmail.com>
2024-11-18 09:28:39 -0500

    avance

---
+++ src/main/java/com/examenparcial/chirinos/ChirinosApplication.java
@@ -0,0 +1,13 @@
+package com.examenparcial.chirinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirinosApplication.class, args);
+	}
+
+}
